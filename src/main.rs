17906d81cfde12e1bbc22de75971e629a309c1eb
Róbert Fuszenecker <robert.fuszenecker@betssongroup.com>
2020-11-26 09:14:13 +0100

    Port fix.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse, http};
 use std::collections::HashMap;
+use std::env;
 use http::StatusCode;
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse, http};
+
+use actix_web::middleware::Logger;
 
 include!(concat!(env!("OUT_DIR"), "/generated.rs"));
 
@@ -9,24 +12,35 @@ async fn greet(req: HttpRequest) -> impl Responder {
     format!("Hello {}!", &name)
 }
 
-/// Valami.
-///
-/// ```
-/// let z = 100
-/// ```
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| "3001".to_string())
+        .parse()
+        .expect("PORT must be a number");
+
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     HttpServer::new(|| {
         let generated = generate();
         App::new()
+            // Setup logging:
+            .wrap(Logger::default())
+
+            // Redirection
+            .route("/", web::get().to(|| { HttpResponse::build(StatusCode::PERMANENT_REDIRECT)
+                .set_header(http::header::LOCATION, "/ui").finish() } ))
+
+            // Static SPA content:
             .service(actix_web_static_files::ResourceFiles::new(
                 "/ui", generated,
             ))
+
+            // API:
             .route("/api/{name}", web::get().to(greet))
-            .route("/", web::get().to(|| { HttpResponse::build(StatusCode::PERMANENT_REDIRECT)
-                .set_header(http::header::LOCATION, "/ui").finish() } ))
-            })
-    .bind("127.0.0.1:8080")?
+        })
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
